a021abaaebd6e4c0a47ebbd954ee780fcd210a79
= <pezi10002@hotmail.com>
2021-03-02 22:15:27 +0100

    config updated, npm run watch IDE + php artisan serve in cmd to live reload changes after save

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
-    ]);
+    ]).browserSync('http://127.0.0.1:8000/');
 
-    //.browserSync('http://127.0.0.1:8000/');
